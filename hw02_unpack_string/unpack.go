fb6d73053b2e5b79678a9edf063b42e8bab8cb44
raptor72 <mctaras51@yandex.ru>
2022-05-14 16:10:23 +0400

    HW2 is complited

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,45 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
-var ErrInvalidString = errors.New("invalid string")
+const winSlash = 92 // mean "\"
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+var (
+	ErrInvalidString   = errors.New("invalid string")
+	ErrInvalidEscaping = errors.New("slash before a wrong simbol")
+)
+
+func Unpack(str string) (string, error) {
+	runes := []rune(str)
+	var sb strings.Builder
+	for i := 0; i < len(runes); {
+		if unicode.IsDigit(runes[i]) {
+			return "", ErrInvalidString
+		}
+		if runes[i] == winSlash {
+			if i < len(runes)-1 && !(unicode.IsDigit(runes[i+1]) || runes[i+1] == winSlash) {
+				return "", ErrInvalidEscaping
+			}
+			i++
+		}
+		if i < len(runes)-1 {
+			if unicode.IsDigit(runes[i+1]) {
+				num, _ := strconv.Atoi(string(runes[i+1]))
+				ss := strings.Repeat(string(runes[i]), num)
+				sb.WriteString(ss)
+				i += 2
+			} else {
+				sb.WriteRune(runes[i])
+				i++
+			}
+		} else {
+			sb.WriteRune(runes[i])
+			i++
+		}
+	}
+	return sb.String(), nil
 }
