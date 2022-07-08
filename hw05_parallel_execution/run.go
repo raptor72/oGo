940508a926a0e2735dafcb0ee04caad94ba8c92f
raptor72 <mctaras51@yandex.ru>
2022-07-08 23:32:18 +0400

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,45 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	taskChannel := make(chan Task)
+	wg := new(sync.WaitGroup)
+	mu := new(sync.Mutex)
+
+	errorCounter := 0
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for task := range taskChannel {
+				err := task()
+				if err != nil {
+					mu.Lock()
+					errorCounter++
+					mu.Unlock()
+				}
+			}
+		}()
+	}
+
+	for _, task := range tasks {
+		if errorCounter >= m {
+			break
+		}
+		taskChannel <- task
+	}
+	close(taskChannel)
+	wg.Wait()
+
+	if errorCounter >= m {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
 }
--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -67,4 +67,17 @@ func TestRun(t *testing.T) {
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
 	})
+
+	t.Run("test errors limit 0", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+		tasks = append(tasks, func() error {
+			return nil
+		})
+
+		workersCount := 4
+		maxErrorsCount := 0
+		err := Run(tasks, workersCount, maxErrorsCount)
+		require.Equal(t, err, ErrErrorsLimitExceeded)
+	})
 }
