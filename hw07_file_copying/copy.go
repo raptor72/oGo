f31bf7fb3b9fd41ac08f9397e009298d584e34c2
Тарасов Максим Сергеевич <mstarasov@domclick.ru>
2022-07-20 11:55:41 +0300

    make a mvp for hw07 [ci skip]

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,104 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrDadSourceFile         = errors.New("source file is not exists")
+	ErrBadDestination        = errors.New("bad destination to copy")
 )
 
+type filePath struct {
+	absPath  string
+	filename string
+	size     int64
+}
+
+func checkCondition(fromPath, toPath string, offset int64) (filePath, filePath, error) {
+	from, to := filePath{}, filePath{}
+	if srcInfo, err := os.Stat(fromPath); err != nil {
+		return from, to, err
+	} else {
+		if srcInfo.IsDir() {
+			return from, to, ErrDadSourceFile
+		}
+		if offset > srcInfo.Size() {
+			return from, to, ErrOffsetExceedsFileSize
+		}
+		from.absPath, err = filepath.Abs(path.Dir(fromPath))
+		if err != nil {
+			return from, to, err
+		}
+		from.filename = srcInfo.Name()
+		from.size = srcInfo.Size()
+	}
+	if _, err := os.Stat(toPath); errors.Is(err, os.ErrNotExist) {
+		return from, to, ErrBadDestination
+	} else {
+		to.absPath, err = filepath.Abs(path.Dir(toPath))
+		if err != nil {
+			return from, to, err
+		}
+	}
+	return from, to, nil
+}
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	from, to, err := checkCondition(fromPath, toPath, offset)
+	if err != nil {
+		return err
+	}
+
+	srcReader, err := os.Open(path.Join(from.absPath, from.filename))
+	if err != nil {
+		fmt.Println("srcReader err")
+		return err
+	}
+
+	//if offset != 0 {
+	//	_, err = srcReader.Seek(offset, 0)
+	//	if err != nil {
+	//		return err
+	//	}
+	//}
+
+	dstPath := path.Join(to.absPath, from.filename)
+	newFile, err := os.Create(dstPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(newFile.Name())
+
+	dstWriter, err := os.OpenFile(newFile.Name(), os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		fmt.Println("dstWriter error")
+		return err
+	}
+
+	if limit == 0 || limit > from.size {
+		limit = from.size
+	}
+
+	byteLen, err := io.CopyN(dstWriter, srcReader, limit)
+	if err != nil {
+		fmt.Println("io.CopyN error")
+		return err
+	}
+	fmt.Printf("Copy %v bytes\n", byteLen)
+
+	//fmt.Println(from.absPath)
+	//fmt.Println(from.filename)
+	//fmt.Println(to.absPath)
+	//fmt.Println(to.filename)
+
 	return nil
 }
--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 )
 
 var (
@@ -10,13 +11,14 @@ var (
 )
 
 func init() {
-	flag.StringVar(&from, "from", "", "file to read from")
-	flag.StringVar(&to, "to", "", "file to write to")
+	flag.StringVar(&from, "from", "testdata/input.txt", "file to read from")
+	flag.StringVar(&to, "to", ".", "file to write to")
 	flag.Int64Var(&limit, "limit", 0, "limit of bytes to copy")
 	flag.Int64Var(&offset, "offset", 0, "offset in input file")
 }
 
 func main() {
 	flag.Parse()
-	// Place your code here.
+	err := Copy(*&from, *&to, *&limit, *&offset)
+	fmt.Printf("Got err %v in main\n", err)
 }
