7a016dce94bc916ec566e6a3a574c53e4e9110e1
raptor72 <mctaras51@yandex.ru>
2022-08-10 22:58:40 +0400

    Added tests

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"io/ioutil"
 	"log"
 	"os"
@@ -16,6 +17,65 @@ func TestReadEmptyDir(t *testing.T) {
 		log.Fatal(err)
 	}
 	defer os.RemoveAll(dir)
+	result, err := ReadDir(dir)
+	require.NoError(t, err)
+	require.Equal(t, expected, result)
+}
+
+func TestReadOk(t *testing.T) {
+	expectedContent := "Bar"
+	env := EnvValue{Value: expectedContent, NeedRemove: false}
+
+	dir, err := ioutil.TempDir(".", "tempdir")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+	tmpOut, err := os.CreateTemp(dir, "out")
+	if err != nil {
+		log.Println(err)
+	}
+	expected := make(Environment)
+
+	tmpOutInfo, err := os.Stat(tmpOut.Name())
+	if err != nil {
+		log.Println(err)
+	}
+	expected[tmpOutInfo.Name()] = env
+	os.Unsetenv(tmpOutInfo.Name())
+	datawriter := bufio.NewWriter(tmpOut)
+	datawriter.WriteString(expectedContent)
+	datawriter.Flush()
+	result, err := ReadDir(dir)
+	require.NoError(t, err)
+	require.Equal(t, expected, result)
+}
+
+func TestRemoveEmpty(t *testing.T) {
+	expectedContent := ""
+	env := EnvValue{Value: expectedContent, NeedRemove: true}
+
+	dir, err := ioutil.TempDir(".", "tempdir")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+	tmpOut, err := os.CreateTemp(dir, "out")
+	if err != nil {
+		log.Println(err)
+	}
+
+	expected := make(Environment)
+
+	tmpOutInfo, err := os.Stat(tmpOut.Name())
+	if err != nil {
+		log.Println(err)
+	}
+	expected[tmpOutInfo.Name()] = env
+	os.Unsetenv(tmpOutInfo.Name())
+	datawriter := bufio.NewWriter(tmpOut)
+	datawriter.WriteString(expectedContent)
+	datawriter.Flush()
 	result, err := ReadDir(dir)
 	require.NoError(t, err)
 	require.Equal(t, expected, result)
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,29 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
 
-func TestRunCmd(t *testing.T) {
-	// Place your code here
+	"github.com/stretchr/testify/require"
+)
+
+func TestOkRunCmd(t *testing.T) {
+	expectedContent := "Bar"
+	env := EnvValue{Value: expectedContent, NeedRemove: false}
+	expected := make(Environment)
+	os.Unsetenv("arg1")
+	expected["arg1"] = env
+	code := RunCmd([]string{"echo", ""}, expected)
+	require.Equal(t, os.Getenv("arg1"), expectedContent)
+	require.Equal(t, 0, code)
+}
+
+func TestPurgeOldValue(t *testing.T) {
+	expectedContent := ""
+	env := EnvValue{Value: expectedContent, NeedRemove: true}
+	expected := make(Environment)
+	expected["arg1"] = env
+	code := RunCmd([]string{"echo"}, expected)
+	require.Equal(t, os.Getenv("arg1"), expectedContent)
+	require.Equal(t, 0, code)
 }
