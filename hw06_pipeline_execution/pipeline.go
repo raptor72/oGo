0dd9169596e06f75a76851a0fca88c662fa922d1
Тарасов Максим Сергеевич <mstarasov@domclick.ru>
2022-08-31 23:06:23 +0400

    hw06 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,32 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	next := in
+	for _, stage := range stages {
+		if stage == nil {
+			continue
+		}
+		next = terminator(done, stage(next))
+	}
+	return next
+}
+
+func terminator(terminate, in In) Out {
+	next := make(Bi)
+
+	go func() {
+		defer close(next)
+		for {
+			select {
+			case value, ok := <-in:
+				if !ok {
+					return
+				}
+				next <- value
+			case <-terminate:
+				return
+			}
+		}
+	}()
+	return next
 }
